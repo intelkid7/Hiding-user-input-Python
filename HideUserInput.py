86fade1537789a1aa9000eadf897f2b883af9297
intelkid7 <108622890+intelkid7@users.noreply.github.com>
2022-08-18 09:38:03 +0530

    Create HideUserInput.py

---
+++ HideUserInput.py
@@ -0,0 +1,6 @@
+#Python program to hide user input
+#CODE
+import getpass
+#if you don't have getpass open command prompt and write the following command - "pip install getpass"
+
+variable = getpass.getpass("User Input:")
